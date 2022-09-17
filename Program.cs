c1c6b34e540d9f4f761dc9d7827e135f4cb00de6
Thiago Bezerra de Carvalho <tb161372@gmail.com>
2022-09-16 22:16:52 -0300

    finalmente

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Ajeitar essa arma aí...");
+Console.ReadKey();
+Console.WriteLine("Aponta ela...");
+Console.ReadKey();
+Console.WriteLine("METE BALA DOIDO!");
+Console.ReadKey();
